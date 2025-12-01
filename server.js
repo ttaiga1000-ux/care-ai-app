762c95183ecbfe7569bc1213ec62d95331f953c5
TANAKA-available <t.taiga1000@gmail.com>
2025-12-01 21:36:46 +0900

    Initial commit with express and dependencies

---
+++ server.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';  // ファイルの先頭に追加
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import advice from "./api/advice.js";
+
+const app = express();
+app.use(bodyParser.json());
+
+// API
+app.post("/api/advice", advice);
+
+// フロント
+app.use(express.static("dist"));
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
